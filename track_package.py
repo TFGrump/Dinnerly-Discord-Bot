2155d41b23fe923331c2af3aa38473817843babb
TFGrump <43284735+TFGrump@users.noreply.github.com>
2020-10-31 13:27:16 -0400

    Update track_package.py

--- track_package.py
+++ track_package.py
@@ -4,8 +4,9 @@ import json
 from time import sleep
 from discord.ext.commands import Bot
 import discord.ext.commands
+
 bot = Bot(command_prefix="_food_")
-TOKEN = ''  # Token for the discord bot
+TOKEN = '' # Token for the discord bot would go here
 
 tracking_numbers = []
 
@@ -15,14 +16,21 @@ async def find_package(ctx, tracking_number):
     try:
         with urllib.request.urlopen(json_url) as url:
             data = json.loads(url.read().decode())
-            if data["Events"][0]["EventType"] == "Delivered":
+            if data["Events"][0]["EventType"] == "Miscellaneous":
+                if data["Events"][1]["EventType"] == "Delivered":
+                    tracking_numbers.remove(tracking_number)
+                    await ctx.send("You food box has been delivered")
+            elif data["Events"][0]["EventType"] == "Delivered":
                 tracking_numbers.remove(tracking_number)
                 await ctx.send("You food box has been delivered")
+            else:
+                print("not here yet")
     except urllib.error.HTTPError:
         await ctx.send("There was an error finding the tracking number")
 
 
 @bot.command(description='Sends a message if a package has arrived',
+             breif='Sends a message if a package has arrived',
              pass_context=True)
 async def track_package(ctx, arg):
     if tracking_numbers.count(arg) == 0:
@@ -30,9 +38,25 @@ async def track_package(ctx, arg):
     while len(tracking_numbers) > 0:
         if tracking_numbers.count(arg) > 0:
             for tracking_number in tracking_numbers:
+                print("checking for package")
                 await find_package(ctx, tracking_number)
+        if len(tracking_numbers) > 0:
+            sleep(1800)
+    print('finished tracking')
 
-        sleep(1800)
+
+@bot.command(description='Stops tracking a certain package',
+             brief='Stops tracking a certain package')
+async def stop_tracking(arg):
+    tracking_numbers.remove(arg)
+
+
+@bot.command(description='Shows what packages are being tracked',
+             brief='Shows what packages are being tracked',
+             pass_context=True)
+async def show_tracking_numbers(ctx):
+    # Create some kind of embed to make the list look nice
+    print('does nothing for now')
 
 
 @bot.event
@@ -47,6 +71,9 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+                                                        name='for your box'))
 
 
 bot.run(TOKEN)
